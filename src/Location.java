63c0ba80747e0744bb69f50f7dcc65019502f1b7
22ug3-0683-sasindu <22ug3-0683@sltc.ac.lk>
2025-10-27 21:32:47 +0530

    Member 2 Added Location Class

---
+++ src/Location.java
@@ -0,0 +1,38 @@
+public class Location {
+    private String id;        
+    private String name;      
+    private String description; 
+
+    public Location(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String toString() {
+        return id + " - " + name + " (" + description + ")";
+    }
+}
