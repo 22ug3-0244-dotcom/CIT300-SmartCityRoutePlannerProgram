6d35fa625f94d8b2d1904e9f8a8c848881d092ce
22ug3-0244-dotcom <22ug3-0244@sltc.ac.lk>
2025-10-28 15:35:13 +0530

    Errors cleard by Member 1 (leader)

--- src/SmartCityRoutePlanner.java
+++ src/SmartCityRoutePlanner.java
@@ -3,6 +3,6 @@ public class SmartCityRoutePlanner {
     public static void main(String[] args) {
         System.out.println("=== SMART CITY ROUTE PLANNER ===");
         MenuInterface menu = new MenuInterface();
-        menu.showMenu(); // run the menu
+        menu.showMenu(); 
     }
 }
